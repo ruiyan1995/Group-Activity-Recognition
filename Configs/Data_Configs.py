c31f66497c87889847141f361e33220f72f1df32
Rui Yan <ruiyan@njust.edu.cn>
2019-03-27 17:59:54 +0800

    Update Data_Configs.py

--- Configs/Data_Configs.py
+++ Configs/Data_Configs.py
@@ -19,28 +19,26 @@ class Data_Configs(object):
             'action': {
                 # you can set it as the case may be, such as 'img', 'npy',
                 # 'hdf5', and so on.
-                'data_type': 'img',
-                'cur_folder': {
-                    'trainval_action': 'person_imgs',
-                    'extract_action_feas': 'person_imgs',
-                    'frame_trainval_activity': 'imgs'
+                'data_type': {
+                    'trainval_action': 'img',
+                    'extract_action_feas': 'img',
                 },
-                'label_types': {'trainval_action': ['action'], 'extract_action_feas': ['activity'], 'frame_trainval_activity': ['frame_activity']},
-                'batch_size': {'trainval_action': {'trainval': 100, 'test': 10}, 'extract_action_feas': {'trainval': 60, 'test': 60} }
+                'cur_folder': {
+                    'trainval_action': 'imgs_ranked',
+                    'extract_action_feas': 'imgs_ranked',
+                },
+                'label_type': {'trainval_action': 'action', 'extract_action_feas': 'activity'},
+                'batch_size': {'trainval_action': {'trainval': 300, 'test': 10}, 'extract_action_feas': {'trainval': 120, 'test': 120} }
             },
             'activity': {
-                'data_type': 'npy',
-                'cur_folder': {
-                    'trainval_activity': 'feas'
+                'data_type': {
+                    'trainval_activity': 'npy',
                 },
-                'label_types': {'trainval_activity': ['activity']},
-                'batch_size': {'trainval_activity': {'trainval': 1000, 'test': 10}}
-            },
-            'semantic': {
-                'data_type': 'npy',
-                'cur_folder': {'trainval_activity': 'feas'},
-                'label_types': {'trainval_activity': ['TF_IDF']},
-                'batch_size': {'trainval': 10, 'test': 1000}
+                'cur_folder': {
+                    'trainval_activity': 'feas',
+                },
+                'label_type': {'trainval_activity': 'activity', 'end_to_end':'activity'},
+                'batch_size': {'trainval_activity': {'trainval': 500, 'test': 10}}
             }
         }
 
@@ -53,7 +51,6 @@ class Data_Configs(object):
                             default=dataset_confs['batch_size'][self.mode])
 
         parser.add_argument('--data_type', type=str, default=dataset_confs[
-                            'data_type'], choices=['img', 'hdf5', 'npy'], help='the story type for data')
-        parser.add_argument('--label_types', type=list, default=dataset_confs['label_types'][self.mode], choices=[['action'], ['action_feas'], [
-                            'temporal_action_feas'], ['CNNLSTM_action_feas'], ['action_feas', 'frame_feas']], help='the label types for data')
+                            'data_type'][self.mode], choices=['img', 'hdf5', 'npy'], help='the story type for data')
+        parser.add_argument('--label_type', type=str, default=dataset_confs['label_type'][self.mode], help='the label type for data')
         return parser.parse_args()
