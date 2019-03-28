8a258c86a829d38f0bd5dedcdd3fdec666fdcb88
Rui Yan <ruiyan@njust.edu.cn>
2019-03-28 17:47:44 +0800

    Update Activity_Level.py

--- Runtime/Activity_Level.py
+++ Runtime/Activity_Level.py
@@ -1,12 +1,20 @@
 from Piplines import *
 class Activity_Level(Piplines):
-	"""docstring for Action_Level"""
-	def __init__(self, dataset_root, dataset_name, mode):
-		super(Activity_Level, self).__init__(dataset_root, dataset_name, 'activity', mode)
+    """docstring for Activity_Level"""
+    def __init__(self, dataset_root, dataset_name, mode):
+        super(Activity_Level, self).__init__(dataset_root, dataset_name, 'activity', mode)
 
 
-	def loadModel(self, pretrained=False):
-		if 'trainval' in self.mode:
-			pretrained=False
-			net = Models.PCTDM(pretrained=pretrained, model_confs=self.model_confs)
-		return net
+    def loadModel(self, pretrained=False):
+        if 'trainval' in self.mode:
+            pretrained=False
+            net = Models.PCTDM(pretrained=pretrained, model_confs=self.model_confs)
+        return net
+    
+    def evaluate(self):
+        pretrained_dict = torch.load('./weights/'+self.dataset_name+'/activity/best_wts.pkl')
+        self.net = Models.PCTDM(pretrained=False, model_confs=self.model_confs)
+        self.net.load_state_dict(pretrained_dict)
+        self.net.eval()
+        self.solver.evaluate()
+
